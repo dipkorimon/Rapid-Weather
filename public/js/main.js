204185cb8a04e6d503267d66775aa12bc97db6c2
dipkorimon <dipkorimon@gmail.com>
2022-11-27 08:46:33 +0600

    updated

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+const city_name = document.getElementById('city_name')
+const temp = document.getElementById('temp');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
+const temp_real_val = document.getElementById('temp_real_val');
+
+const getInfo = async(event) => {
+    event.preventDefault();
+    let cityVal = cityName.value;
+    if (cityVal === "") {
+        city_name.innerText = `Please write city name before search`;
+        datahide.classList.add('data_hide');
+    } else {
+        try {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=218e0af13968de702922aa542f9bb10a`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const arrData = [data];
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            const tempMood = arrData[0].weather[0].main;
+            if (tempMood == "Clear") {
+                temp_status.innerHTML = "<i class='fa-solid fa-sun'></i>";
+            } else if (tempMood == "Clouds") {
+                temp_status.innerHTML = "<i class='fa-solid fa-cloud'></i>";
+            } else if (tempMood == "Rain") {
+                temp_status.innerHTML = "<i class='fa-solid fa-rain'></i>";
+            }
+            datahide.classList.remove('data_hide');
+        } catch {
+            city_name.innerText = `Please write city name properly`;
+            datahide.classList.add('data_hide');
+        }
+    }
+}
+
+submitBtn.addEventListener('click', getInfo);
