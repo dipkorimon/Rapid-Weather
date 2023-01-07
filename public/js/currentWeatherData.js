157dba2c6edc808b302199144c90addc5a0d1f9e
dipkorimon <dipkorimon@gmail.com>
2023-01-07 22:16:13 +0600

    current weather page modified

--- public/js/currentWeatherData.js
+++ public/js/currentWeatherData.js
@@ -12,21 +12,20 @@ const gust = document.getElementById('gust');
 const description = document.getElementById('description');
 const pressure = document.getElementById('pressure');
 const humidity = document.getElementById('humidity');
-const country = document.getElementById('country');
 
 
 const getInfo = async(event) => {
     event.preventDefault();
     let cityVal = cityName.value;
     if (cityVal === "") {
-        city_name.innerText = `Please write city name before search`;
+        city_name.innerText = `Please write city name before search!`;
     } else {
         try {
             let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=12dbf2d699361c286a404aad012baf20`;
             const response = await fetch(url);
             const data = await response.json();
             const arrData = [data];
-            city_name.innerText = arrData[0].name;
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
             temp.innerText = arrData[0].main.temp;
             temp_status.innerText = arrData[0].weather[0].main;
             temp_min.innerText = arrData[0].main.temp_min;
@@ -38,9 +37,8 @@ const getInfo = async(event) => {
             description.innerText = arrData[0].weather[0].description;
             pressure.innerText = arrData[0].main.pressure;
             humidity.innerText = arrData[0].main.humidity;
-            country.innerText = arrData[0].sys.country;
         } catch {
-            city_name.innerText = `Please write city name properly`;
+            city_name.innerText = `Please write city name properly!`;
         }
     }
 }
